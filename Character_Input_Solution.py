22ec354cb2526505898bd9b6b06489d5d2e18dd0
Tao <34307060+sosotaoge@users.noreply.github.com>
2018-09-20 14:39:14 +0800

    Update Character_Input_Solution.py

--- Character_Input_Solution.py
+++ Character_Input_Solution.py
@@ -7,6 +7,6 @@ year = int(year)
 
 name = input("What's your name: ")
 age = input("How old are you: ")
-y100 = year + 100 - age									# Calculate the year when getting 100 years old
+y100 = year + 100 - age									      # Calculate the year when getting 100 years old
 
 print(name + " you will turn 100 years old in the year " + str(y100))
